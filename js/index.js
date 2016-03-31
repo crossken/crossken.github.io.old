afcacc9bbd8ff834d93ae5e3f4ff4e62db1210d3
kenchung <kenchung.ftd@gmail.com>
2016-03-31 15:37:44 +0800

    test

--- js/index.js
+++ js/index.js
@@ -30,6 +30,8 @@ window.onload = function(){
 					}
 					if (swiper.activeIndex == 2) {
 
+						$('#s2-title').addClass('fadeInUp animated');
+
 						setTimeout(function(){
 							drawHexagon();
 						},700)
@@ -239,27 +241,27 @@ function drawHexagon(){
 }
 
 
-var nowProgress2 = document.getElementById('nowprogress');
-var loadingTimer2 = setInterval(function(){
-	if (isTo80) {
-		var pbWidth2 = parseFloat(nowProgress2.style.width);
-		if (pbWidth2<88.5) {
-			nowProgress2.style.width = pbWidth2 + 0.5 +'%';
-		} else {
-			clearInterval(loadingTimer2);
-			setTimeout(function(){
-				$('.loading').fadeOut(500,function(){
-					setTimeout(function(){
-						$('.sub').animate({'height':windowWidth*1.15,'top':0}, 20000);
-					},200);
-					setTimeout(function(){
-						mainSwiper.slideTo(1, 1500,true);
-					},21000)
-				});
-			},300)
-		}
-	}
-},15);
+// var nowProgress2 = document.getElementById('nowprogress');
+// var loadingTimer2 = setInterval(function(){
+// 	if (isTo80) {
+// 		var pbWidth2 = parseFloat(nowProgress2.style.width);
+// 		if (pbWidth2<88.5) {
+// 			nowProgress2.style.width = pbWidth2 + 0.5 +'%';
+// 		} else {
+// 			clearInterval(loadingTimer2);
+// 			setTimeout(function(){
+// 				$('.loading').fadeOut(500,function(){
+// 					setTimeout(function(){
+// 						$('.sub').animate({'height':windowWidth*1.15,'top':0}, 20000);
+// 					},200);
+// 					setTimeout(function(){
+// 						mainSwiper.slideTo(1, 1500,true);
+// 					},21000)
+// 				});
+// 			},300)
+// 		}
+// 	}
+// },15);
 
 $('.sub').height(parseInt(windowWidth*0.1));
 $('.sub').css('top', parseInt(windowWidth*0.6));
